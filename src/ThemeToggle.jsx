3708cd58f59ae3c96a6dc2b6c398a80d469c189c
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-21 12:39:45 +0530

    Set Theme Color Variables

--- src/ThemeToggle.jsx
+++ src/ThemeToggle.jsx
@@ -1,6 +1,17 @@
 import { useGlobalContext } from './context';
 import { BsFillMoonFill, BsFillSunFill } from 'react-icons/bs';
 const ThemeToggle = () => {
-  return <div>ThemeToggle</div>;
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
+  return (
+    <section className="toggle-container">
+      <button className="dark-toggle" onClick={toggleDarkTheme}>
+        {isDarkTheme ? (
+          <BsFillSunFill className="dota"></BsFillSunFill>
+        ) : (
+          <BsFillMoonFill className="dota-moon"></BsFillMoonFill>
+        )}
+      </button>
+    </section>
+  );
 };
 export default ThemeToggle;
