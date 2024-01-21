80ba74854b48559779009a76e5d438358ecef463
Ruturaj2003 <ruturajhb2003@gmail.com>
2024-01-21 19:09:20 +0530

    Fix Some Bug

--- src/Gallery.jsx
+++ src/Gallery.jsx
@@ -3,34 +3,57 @@ import { url } from './api';
 import axios from 'axios';
 
 const Gallery = () => {
-  const resp = useQuery({
+  const response = useQuery({
     queryKey: ['images'],
     queryFn: async () => {
-      const result = await axios.get(url);
+      const result = await axios.get(`${url}&query=cat`);
       return result.data;
     },
   });
+  if (response.isLoading) {
+    return <h3>Loading Data Please Wait</h3>;
+  }
+  if (response.error) {
+    return <h3> There was An Error</h3>;
+  }
+  const ImgArray = response.data.results;
+  console.log(ImgArray);
 
-  const ImgArry = resp.data || [];
-  console.log(ImgArry);
+  ImgArray.map((item) => {
+    console.log(item.id);
+  });
 
   return (
     <div id="dotacont" className="image-container">
-      {ImgArry.map((img) => {
-        const dec = img.alt_description;
-        console.log(img);
+      {ImgArray.map((item) => {
+        const des = item.alt_description.slice(0, 40) + '...';
+        console.log(item);
         return (
-          <div key={img.id} style={{ position: 'relative' }}>
-            <img
-              className="img"
-              alt={img.alt_description}
-              src={img.urls.regular}
-            ></img>
-            <legend>{dec.slice(0, 30) + '...'}</legend>
+          <div key={item.id} style={{ position: 'relative' }}>
+            <img className="img" src={item?.urls?.regular} alt="" />
+            <legend>{des}</legend>
           </div>
         );
       })}
     </div>
   );
+  // return (
+  //   <div id="dotacont" className="image-container">
+  //     {ImgArry.map((img) => {
+  //       const dec = img.alt_description;
+  //       console.log(img);
+  //       return (
+  //         <div key={img.id} style={{ position: 'relative' }}>
+  //           <img
+  //             className="img"
+  //             alt={img.alt_description}
+  //             src={img.urls.regular}
+  //           ></img>
+  //           <legend>{dec.slice(0, 30) + '...'}</legend>
+  //         </div>
+  //       );
+  //     })}
+  //   </div>
+  // );
 };
 export default Gallery;
--- src/api.jsx
+++ src/api.jsx
@@ -1,4 +1,3 @@
 const axcsKey = 'RlNk9iUoNEMbSc8-EnQy6X4C2A2PPvJ8AZWF4CUZhhY';
 
-export const url = `https://api.unsplash.com/photos/?client_id=${axcsKey}`;
-export const urlPic = `${url}`;
+export const url = `https://api.unsplash.com/search/photos?client_id=${axcsKey}`;
